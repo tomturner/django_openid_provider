b2bc665b2f858fa058c9c36bf0fe1a18589222c5
Shay Erlichmen <erlichmen@gmail.com>
2013-06-18 23:37:24 +0300

    only init the temp file if file store is used

--- openid_provider/conf.py
+++ openid_provider/conf.py
@@ -1,14 +1,15 @@
 import os
-import tempfile
 from django.conf import settings
-
-tempdir = tempfile.gettempdir()
 
 STORE = getattr(settings, 'OPENID_PROVIDER_STORE',
                 'openid.store.filestore.FileOpenIDStore')
 
-FILESTORE_PATH = getattr(settings, 'OPENID_PROVIDER_FILESTORE_PATH',
-                         os.path.join(tempdir, 'openid-filestore'))
+if STORE == 'openid.store.filestore.FileOpenIDStore':
+    import tempfile
+    tempdir = tempfile.gettempdir()
+    
+    FILESTORE_PATH = getattr(settings, 'OPENID_PROVIDER_FILESTORE_PATH',
+                             os.path.join(tempdir, 'openid-filestore'))
 
 SREG_DATA_CALLBACK = getattr(settings, 'OPENID_PROVIDER_SREG_DATA_CALLBACK',
                              'openid_provider.utils.get_default_sreg_data')
